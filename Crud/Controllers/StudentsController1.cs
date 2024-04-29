1de57c0618979fed919c610f3319b5a5bb7f7574
Jerrtertytwo <amadajerryboycorselis@gmail.com>
2024-04-29 16:27:40 +0800

    Add project files.

---
+++ Crud/Controllers/StudentsController1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Crud.Controllers
+{
+    public class StudentsController1 : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(AddStudentViewModel viewModel)
+        {
+            return View();
+       }
+            
+    }
+}
---
+++ Crud/Data/Applicationdbcontext.cs
@@ -0,0 +1,14 @@
+﻿using Crud.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Data
+{
+    public class Applicationdbcontext : DbContext
+    {
+        public Applicationdbcontext(DbContextOptions<Applicationdbcontext> Options) : base(Options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
